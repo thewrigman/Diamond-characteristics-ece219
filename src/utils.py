b1f7183514b4e5cee3f3a66b1da13252a6ce55a8
ineshc <inesh33@gmail.com>
2023-03-12 15:42:41 -0700

    loading

---
+++ src/utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+import sys
+import pandas as pd
+
+sys.path.append("../project_data")
+def loadData(filePath="../project_data/diamonds.csv"):
+    df = pd.read_csv(filePath)
+    return df
+
