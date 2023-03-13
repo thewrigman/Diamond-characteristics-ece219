53053c554dd9f1eca041f838be324b2616d72409
ineshc <inesh33@gmail.com>
2023-03-12 20:03:49 -0700

    standard

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import numpy as np
 import sys
 import pandas as pd
 
+################################################################################
 sys.path.append("../project_data")
 def loadData(filePath="../project_data/diamonds.csv"):
     df = pd.read_csv(filePath)
