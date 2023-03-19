4541717e0050a0a267f5d67a45b0d2574c020102
ineshc <inesh33@gmail.com>
2023-03-19 13:20:38 -0700

    have to do 8.3, poly table, NN figs

--- src/mlp.py
+++ src/mlp.py
@@ -3,45 +3,65 @@ from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import mean_squared_error
 from sklearn.neural_network import MLPRegressor
 
+
 if __name__ == '__main__':
 
     f = open("mlpLogs.txt", "a")
-    funcNames = ["Nothing","Logrithm"]
-    remColsGrid = [0]
+    
+    sizeLayer = [50,100,200]
+    alphas = [0.01, 0.1]
+    numLayers = [4,8,12]
     kf = StratifiedKFold(n_splits=10, shuffle=False)
-    for remColParam in remColsGrid:
-        df=loadData(quant=True,unSkew=True,remCols=remColParam)
-        X = df.drop(columns=['price'])
-        Y = df['price']
-        totalTrainRSME = 0
-        totalTestRSME = 0
-        f.write("---------------------------------------------------------\n")
-        f.write(f"Num Columns Removed: {remColParam}\n")
-        for train_index, test_index in kf.split(X, Y.to_numpy()):
-            X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-            y_train, y_test = Y.iloc[train_index], Y.iloc[test_index]
-            X_train, X_test = scaleTrainTest(X_train, X_test)
+    unSkewParam = True
+    remColParam = 0
+    
 
-            reg = MLPRegressor(activation='relu',
-                               solver='adam',
-                               alpha=0.001,
-                               ).fit(X_train, y_train)
+    df=loadData(quant=True,unSkew=unSkewParam,remCols=remColParam)
+    X = df.drop(columns=['price'])
+    Y = df['price']
 
-            trainPred = 0
-            testPred = 0
-            trainRSME = 0
-            testRSME = 0
+    
+    for nL in numLayers:
+        for sL in sizeLayer:
+            f.write(f"----------------------------------{nL} X {sL}-----------------------------\n")
+            for a in alphas:
+                f.write(f"----------------------------------ALPHA = {a}-----------------------------\n")
+                totalTrainRSME = 0
+                totalTestRSME = 0
+                totalOOB = 0
+                for train_index, test_index in kf.split(X, Y.to_numpy()):
+                    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+                    y_train, y_test = Y.iloc[train_index], Y.iloc[test_index]
+                    X_train, X_test = scaleTrainTest(X_train, X_test)
+                    layers = np.ones(nL)*sL
+                    reg = MLPRegressor(hidden_layer_sizes=layers,
+                                    alpha = a,
+                                    solver = 'adam',
+                                    activation = 'relu',
+                                    learning_rate_init=0.001,
+                                    batch_size=400,
+                                    learning_rate='adaptive',
+                                    momentum=0.9,
+                                    early_stopping=True,
+                                    validation_fraction=0.1)
+                    
+                    trainPred = 0
+                    testPred = 0
+                    trainRSME = 0
+                    testRSME = 0
 
-            trainPred = reg.predict(X_train)
-            testPred = reg.predict(X_test)
-            trainRSME = mean_squared_error(trainPred, y_train, squared=False)
-            testRSME = mean_squared_error(testPred, y_test, squared=False)
+                    trainPred = reg.predict(X_train)
+                    testPred = reg.predict(X_test)
+                    trainRSME = mean_squared_error(trainPred, y_train, squared=False)
+                    testRSME = mean_squared_error(testPred, y_test, squared=False)
+                    oob = reg.oob_score_
+                    f.write(f"Split Training RSME: {trainRSME} Testing RSME {testRSME}, OOB Error: {oob}\n")
+                    totalTrainRSME += trainRSME
+                    totalTestRSME += testRSME
+                    totalOOB += oob
 
+                f.write(f"Mean TrainRSME = {totalTrainRSME/10}\n")
+                f.write(f"Mean TestRSME = {totalTestRSME/10}\n")
+                f.write(f"Mean OOB Error = {totalOOB/10}\n")
 
-            f.write(f"Split Training RSME: {trainRSME} Testing RSME {testRSME},\n")
-            totalTrainRSME += trainRSME
-            totalTestRSME += testRSME
-        f.write(f"Mean TrainRSME = {totalTrainRSME/10}\n")
-        f.write(f"Mean TestRSME = {totalTestRSME/10}\n")
-            
     f.close()
